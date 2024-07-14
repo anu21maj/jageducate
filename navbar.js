22f0a5f234359a60f7ea52d528d121f48112fcf1
anu21maj <anushka05m.2002@gmail.com>
2024-07-14 21:40:23 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const dropbtn = document.querySelector('.dropbtn');
+  const dropdownContent = document.querySelector('.dropdown-content');
+
+  dropbtn.addEventListener('click', function(event) {
+      event.preventDefault();
+      dropdownContent.classList.toggle('show');
+  });
+
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+          if (dropdownContent.classList.contains('show')) {
+              dropdownContent.classList.remove('show');
+          }
+      }
+  }
+});
